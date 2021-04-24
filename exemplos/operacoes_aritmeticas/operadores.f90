44bf2eaf6094d1e8b73f52f8c9aba986206720dd
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-04-24 14:51:48 -0300

    Exemplo operadores aritméticos em fortran

---
+++ exemplos/operacoes_aritmeticas/operadores.f90
@@ -0,0 +1,17 @@
+program operadores
+implicit none
+
+integer:: a, b
+
+a = 2
+b = 2
+
+
+write(*,*) "a+b=", a+b
+write(*,*) "a-b=", a-b
+write(*,*) "a*b=", a*b
+write(*,*) "a**b=", a**b
+write(*,*) "a/b=", a/b
+
+
+end program
