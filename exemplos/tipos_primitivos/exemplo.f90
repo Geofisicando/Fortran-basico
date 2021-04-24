20ae64d2651ca327300584b09be6df5fbd244ff0
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-04-24 12:02:36 -0300

    Tipos primitivos em Fortran

---
+++ exemplos/tipos_primitivos/exemplo.f90
@@ -0,0 +1,16 @@
+program exemplo
+implicit none
+
+! Este é um comentário em um programa de exemplo
+! Declaração de variáveis
+
+INTEGER:: i=1 
+REAL:: r=1.5
+
+write(*,*) "i=",i
+write(*,*) "r=",r
+
+write(*,*) 'Erro, qualquer...'
+stop 4
+
+end program
