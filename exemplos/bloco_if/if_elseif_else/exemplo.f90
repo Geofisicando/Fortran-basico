a05f0ba23238c3da3cb92cbe2291583dcae17d40
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-06-18 14:53:00 -0300

    exemplo de bloco if elseif else em fortran

---
+++ exemplos/bloco_if/if_elseif_else/exemplo.f90
@@ -0,0 +1,30 @@
+program exemplo
+implicit none
+
+real:: nota
+
+! nota < 5     INSUFICIENTE
+! 5 < nota < 7 REGULAR
+! 7 < nota < 9 BOM
+! nota > 9     EXCELENTE
+
+write(*,*) "Nota: "
+read(*,*) nota
+
+if (nota<0) then
+	write(*,*) "Nota inválida!"
+	stop 1
+end if
+
+if (nota<5) then
+	write(*,*) "INSUFICIENTE"
+else if (nota < 7) then
+	write(*,*) "REGULAR"
+else if (nota < 9) then
+	write(*,*) "BOM"
+else
+	write(*,*) "EXCELENTE"
+end if
+
+end program
+
